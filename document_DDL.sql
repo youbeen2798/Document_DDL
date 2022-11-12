63b782ef28c022cc22a33b461fb95d27ca9c476f
hobby3530 <jungy3530@gmail.com>
2022-11-12 16:01:13 +0900

    Update document_DDL.sql
    부모 테이블 추가 및 오타 수정 완료

--- document_DDL.sql
+++ document_DDL.sql
@@ -1,5 +1,5 @@
 # 증명서 종류 테이블
-CREATE table cerificate_kind(
+CREATE table certificate_kind(
 	id INT AUTO_INCREMENT,
     certificate_name NVARCHAR(100) NOT NULL,
 	PRIMARY KEY(id)
@@ -16,7 +16,7 @@ CREATE table certificate(
     # 발급일
     issue_date DATE NOT NULL,
     PRIMARY KEY(confirmation_number),
-    CONSTRAINT refer_certificate_id FOREIGN KEY(kind_id) REFERENCES certificate_kind(id)
+    CONSTRAINT certificate_refer_certificate_id FOREIGN KEY(kind_id) REFERENCES certificate_kind(id)
 );
 
 # 장소 테이블
@@ -36,20 +36,6 @@ CREATE TABLE declarant_qualification (
     PRIMARY KEY (id)
 );
 
-#사람 주소 테이블
-CREATE TABLE person_address(
-	id BIGINT AUTO_INCREMENT,
-    #사람 고유 아이디
-    person_id BIGINT NOT NULL,
-   #주소 고유 아이디
-    address_id BIGINT NOT NULL,
-    #신고일
-    report_date DATE NOT NULL,
-    PRIMARY KEY(id),
-    CONSTRAINT refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
-    CONSTRAINT refer_address_id FOREIGN KEY(address_id) REFERENCES address(id)
-); 
-
 # 주소 테이블
 CREATE TABLE address (
 	id BIGINT AUTO_INCREMENT,
@@ -64,7 +50,7 @@ CREATE TABLE person (
     #이름
     name NVARCHAR(100) NOT NULL,
     #주민 등록 번호
-    resident_registration_number CHAR(300) UNIQUE NOT NULL,
+    resident_registration_number TEXT NOT NULL,
     #성별
     gender NCHAR(1) NOT NULL,
     #생일
@@ -72,26 +58,36 @@ CREATE TABLE person (
     #배우자
     spouse BIGINT,
     PRIMARY KEY (id),
-    CONSTRAINT refer_spouse_id FOREIGN KEY (spouse) REFERENCES person(id)
+    CONSTRAINT person_refer_spouse_id FOREIGN KEY (spouse) REFERENCES person(id)
 );
 
-
-# 출생신고서 세부 정보 테이블
-CREATE TABLE birth_detail (
+# 부모 테이블
+CREATE TABLE parents (
+	# 본인 고유 아이디
 	id BIGINT,
-    #출생 날짜 및 시간
-    birth_datetime DATETIME NOT NULL,
-    #태어난 장소 - locaiton.id 참조
-    location_id INT NOT NULL,
-    #신고인 - declarant.id 참조
-    declarant_id BIGINT NOT NULL,
-    #신고 날짜 
-    report_date DATE NOT NULL,
+    # 어머니 고유 아이디
+    mother_id BIGINT,
+    # 아버지 고유 아이디
+    father_id BIGINT,
     PRIMARY KEY (id),
-    CONSTRAINT refer_person_id FOREIGN KEY(id) REFERENCES person(id),
-    CONSTRAINT refer_location_id FOREIGN KEY(location_id) REFERENCES location(id),
-    CONSTRAINT refer_declarant_id FOREIGN KEY(declarant_id) REFERENCES declarant(id)
+    CONSTRAINT parents_refer_id FOREIGN KEY (id) REFERENCES person(id),
+	CONSTRAINT parents_refer_mother_id FOREIGN KEY (mother_id) REFERENCES person(id),
+	CONSTRAINT parents_refer_father_id FOREIGN KEY (father_id) REFERENCES person(id)
 );
+
+#사람 주소 테이블
+CREATE TABLE person_address(
+	id BIGINT AUTO_INCREMENT,
+    #사람 고유 아이디
+    person_id BIGINT NOT NULL,
+   #주소 고유 아이디
+    address_id BIGINT NOT NULL,
+    #신고일
+    report_date DATE NOT NULL,
+    PRIMARY KEY(id),
+    CONSTRAINT person_address_refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
+    CONSTRAINT person_address_refer_address_id FOREIGN KEY(address_id) REFERENCES address(id)
+); 
 
 #신고인 테이블
 CREATE TABLE declarant(
@@ -105,9 +101,28 @@ CREATE TABLE declarant(
     #신고인 전화번호
     phone_number VARCHAR(20),
     PRIMARY KEY(id),
-    CONSTRAINT refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
-	CONSTRAINT refer_declarant_qualification_id FOREIGN KEY(qualification) REFERENCES declarant_qualification(id)
+    CONSTRAINT declarant_refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
+	CONSTRAINT declarant_refer_declarant_qualification_id FOREIGN KEY(qualification) REFERENCES declarant_qualification(id)
 );
+
+# 출생신고서 세부 정보 테이블
+CREATE TABLE birth_detail (
+	id BIGINT,
+    #출생 날짜 및 시간
+    birth_datetime DATETIME NOT NULL,
+    #태어난 장소 - locaiton.id 참조
+    location_id INT NOT NULL,
+    #신고인 - declarant.id 참조
+    declarant_id BIGINT NOT NULL,
+    #신고 날짜 
+    report_date DATE NOT NULL,
+    PRIMARY KEY (id),
+    CONSTRAINT birth_detail_refer_person_id FOREIGN KEY(id) REFERENCES person(id),
+    CONSTRAINT birth_detail_refer_location_id FOREIGN KEY(location_id) REFERENCES location(id),
+    CONSTRAINT birth_detail_refer_declarant_id FOREIGN KEY(declarant_id) REFERENCES declarant(id)
+);
+
+
 
 # 사망신고서 세부 정보 테이블
 CREATE TABLE death_detail(
@@ -123,8 +138,8 @@ CREATE TABLE death_detail(
     #신고일
     report_date DATE NOT NULL,
     PRIMARY KEY(id),
-    CONSTRAINT refer_location_id FOREIGN KEY(location_id) REFERENCES location(id),
-	CONSTRAINT refer_declarant_id FOREIGN KEY(declarant_id) REFERENCES declarant(id)
+    CONSTRAINT death_detail_refer_location_id FOREIGN KEY(location_id) REFERENCES location(id),
+	CONSTRAINT death_detail_refer_declarant_id FOREIGN KEY(declarant_id) REFERENCES declarant(id)
 );
 
 # 세대 테이블
@@ -138,7 +153,7 @@ CREATE TABLE household (
     # 세대 구성 일자
     date_of_composition DATE NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT refer_person_id FOREIGN KEY(head_of_household) REFERENCES person(id)
+    CONSTRAINT household_refer_person_id FOREIGN KEY(head_of_household) REFERENCES person(id)
 );
 
 # 사람 세대 테이블
@@ -154,8 +169,8 @@ CREATE TABLE person_household (
     # 변동 사유
     reason_of_change NVARCHAR(100) NOT NULL,
     PRIMARY KEY (id),
-    CONSTRAINT refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
-    CONSTRAINT refer_household_id FOREIGN KEY(household_id) REFERENCES household(id)
+    CONSTRAINT person_household_refer_person_id FOREIGN KEY(person_id) REFERENCES person(id),
+    CONSTRAINT person_household_refer_household_id FOREIGN KEY(household_id) REFERENCES household(id)
 );
     
     
